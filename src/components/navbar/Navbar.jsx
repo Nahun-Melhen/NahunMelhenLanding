dd61caedbb3f7c9e7422edfbe9bf0ad1d046e9f7
Nahun Melhen <141931060+Nahun-Melhen@users.noreply.github.com>
2024-07-03 10:16:43 -0300

    agregando tipografia y colores designados

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'bootstrap'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1 className='text-warning'>Navbar</h1>
+        
+    </div>
+  )
+}
+
+export default Navbar
